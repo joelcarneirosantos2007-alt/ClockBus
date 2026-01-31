e3b7a08771532167802df70d3f70210e3bdef724
joelcarneirosantos2007-alt <joelcarneirosantos2007@gmail.com>
2026-01-31 01:37:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -16,4 +16,10 @@ if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("./service-worker.js")
     .then(() => console.log("✅ PWA ativado com sucesso"))
     .catch((err) => console.log("Erro no SW:", err));
+// ===== ATIVAR PWA =====
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("./service-worker.js")
+    .then(() => console.log("✅ PWA ativado com sucesso"))
+    .catch((err) => console.log("Erro no SW:", err));
 }
+
