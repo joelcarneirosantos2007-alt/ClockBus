a63797aeda98583382540081000265dce5130ce1
joelcarneirosantos2007-alt <joelcarneirosantos2007@gmail.com>
2026-01-31 00:56:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+body {
+  font-family: Arial, sans-serif;
+  text-align: center;
+  padding: 30px;
+  background: #f2f2f2;
+}
+
+button {
+  padding: 15px 20px;
+  font-size: 18px;
+  border: none;
+  border-radius: 12px;
+  background: black;
+  color: white;
+  cursor: pointer;
+}
+
+button:hover {
+  opacity: 0.8;
+}
 
