5515c43eb330377ef6b7f48816f901a6369e0197
joelcarneirosantos2007-alt <joelcarneirosantos2007@gmail.com>
2026-01-31 00:59:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
-body {
-  font-family: Arial, sans-serif;
-  text-align: center;
-  padding: 30px;
-  background: #f2f2f2;
-}
+// Velocidades médias (km/h)
+let velMediaBus = 18;
+let velMediaPessoa = 4.5;
 
-button {
-  padding: 15px 20px;
-  font-size: 18px;
-  border: none;
-  border-radius: 12px;
-  background: black;
-  color: white;
-  cursor: pointer;
-}
+function usarVelocidadeBus() {
 
-button:hover {
-  opacity: 0.8;
-}
+  let distanciaKm = 5; // exemplo: 5 km
 
+  let tempoHoras = distanciaKm / velMediaBus;
+  let tempoMin = Math.round(tempoHoras * 60);
+
+  document.getElementById("resultado").innerText =
+    "⏳ O ônibus chega em aproximadamente " + tempoMin + " minutos!";
+}
